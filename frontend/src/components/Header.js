200b3e7c99f45fd49f1fd45eb74a407d514febfe
garyarzumanyan <garyarzumanyan@gmail.com>
2022-08-05 23:09:28 -0700

    Cleaned styling on header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/topbar_logo.png";
-import placeholder from "../imgs/placeholder.png"
+import placeholder from "../imgs/placeholder.png";
 
 const LoggedOutView = (props) => {
   if (!props.currentUser) {
